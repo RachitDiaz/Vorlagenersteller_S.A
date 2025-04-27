6cac08bc9279a90611aac9d4b8d104adc5199f2a
Jesus <jesus.menaamador@ucr.ac.cr>
2025-04-26 21:20:29 -0600

    Se añade el proyecto de la Api, además se implementan métodos de calculo en la clase Controller y el modelo de las solicitudes en la clase Model

---
+++ BackEnd/API-MediSeguro/API-MediSeguro/Models/SolicitudMediSeguroMontoModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API_MediSeguro.Models
+{
+    public class SolicitudMediSeguroMontoModel
+    {
+        [Required(ErrorMessage = "La fecha de nacimiento es obligatoria")]
+        public DateTime FechaNacimiento { get; set; }
+
+        [Required(ErrorMessage = "El género es obligatorio")]
+        public string Genero { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "La cantidad de dependientes es obligatoria")]
+        [Range(0, int.MaxValue, ErrorMessage = "La cantidad de dependientes no puede ser negativa")]
+        public int CantidadDependientes { get; set; }
+    }
+}
