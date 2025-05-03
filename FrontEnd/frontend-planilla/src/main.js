8add1f7c1bdde01958c5d3f5cbc5c14ebf316d9b
Fervolk <shihtangdaniel@gmail.com>
2025-05-03 14:20:11 -0600

    Se agrega un modal con el form para agregar beneficio nuevo

--- FrontEnd/frontend-planilla/src/main.js
+++ FrontEnd/frontend-planilla/src/main.js
@@ -1,10 +1,14 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import { createRouter, createWebHistory } from "vue-router";
+import AgregarBeneficios from "./components/AgregarBeneficios.vue";
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 
 const router = createRouter({
   history: createWebHistory(),
-  routes: [],
+  routes: [
+    {path: "/AgregarBeneficios", name: "AgregarBeneficios", component: AgregarBeneficios},
+  ],
 });
 
 createApp(App).use(router).mount('#app')
