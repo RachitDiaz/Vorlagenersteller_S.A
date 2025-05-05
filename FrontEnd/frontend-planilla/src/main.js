8276c1ef9c8550bc46d013df0f2a1b74dfe8e83d
Aldo Cascante <cascante.aldo@gmail.com>
2025-05-04 19:46:42 -0600

    Front-end para ver datos de empresa

--- FrontEnd/frontend-planilla/src/main.js
+++ FrontEnd/frontend-planilla/src/main.js
@@ -5,6 +5,7 @@ import AgregarBeneficios from "./components/AgregarBeneficios.vue";
 import RegistrarDueno from "./components/RegistrarDueno.vue";
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import ListaEmpresas from './components/ListaEmpresas.vue';
+import VerEmpresa from './components/VerEmpresa.vue';
 
 const router = createRouter({
   history: createWebHistory(),
@@ -12,7 +13,7 @@ const router = createRouter({
     { path: "/AgregarBeneficios", name: "AgregarBeneficios", component: AgregarBeneficios, meta: { title: 'Agregar Beneficios' } },
     { path: "/RegistrarDueno", name: "RegistrarDueño", component: RegistrarDueno, meta: { title: 'Registrar Dueño' } },
     { path: "/ListaEmpresas", name: "ListaEmpresa", component: ListaEmpresas, meta: { title: 'Lista de Empresas' } },
-
+    { path: "/VerEmpresa", name: "VerEmpresa", component: VerEmpresa, meta: { title: 'Ver Empresa' } },
   ],
 });
 
