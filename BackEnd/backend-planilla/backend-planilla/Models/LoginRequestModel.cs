db63a0d5c915a05b96054212e496dbae58713323
Jesus <jesus.menaamador@ucr.ac.cr>
2025-05-06 01:05:29 -0600

    Se divide la lógica del código de logincontroller en el model y en la parte de services para la lógica de negocio

---
+++ BackEnd/backend-planilla/backend-planilla/Models/LoginRequestModel.cs
@@ -0,0 +1,9 @@
+﻿namespace backend_planilla.Models
+{
+    public class LoginRequestModel
+    {
+        public string? Correo { get; set; }
+        public string? Contrasena { get; set; }
+        public string? Rol { get; set; }
+    }
+}
