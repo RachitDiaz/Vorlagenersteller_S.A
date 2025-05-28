ffeda482cfad45631f86bd0fc4c9215504c9e528
Fervolk <shihtangdaniel@gmail.com>
2025-05-27 20:26:01 -0600

    Se hace la refactorización de los beneficios. Se cambia el nombre del controller a BeneficiosController Se agregan los Query.cs y Repository.cs para Beneficios

---
+++ BackEnd/backend-planilla/backend-planilla/Application/IBeneficiosQuery.cs
@@ -0,0 +1,12 @@
+﻿using backend_planilla.Domain;
+using backend_planilla.Infraestructure;
+using Microsoft.AspNetCore.Mvc;
+
+namespace backend_planilla.Application
+{
+    public interface IBeneficiosQuery
+    {
+        List<BeneficioModel> GetBeneficios(string correo);
+        bool CrearBeneficio(BeneficioModel beneficio, string correo);
+    }
+}
