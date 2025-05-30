8d3f290b8e45e14b1d9c4b56396d97f187274c90
Aldo Cascante <cascante.aldo@gmail.com>
2025-05-29 23:04:55 -0600

    Script 7: cambio en  foreign keys y stored procedure para edicion

--- DB/scripts/VorlagenerstellerQuery6.sql
+++ DB/scripts/VorlagenerstellerQuery6.sql
@@ -6,9 +6,11 @@ ALTER COLUMN Correo CHAR(60) NOT NULL;
 ALTER TABLE Usuario
 ADD CONSTRAINT UQ_Usuario_Correo UNIQUE (Correo);
 
+
+
 INSERT INTO Empleado(CedulaEmpleado, CedulaEmpresa, Banco, SalarioBruto, TipoContrato, UsuarioCreador, UltimoEnModificar)
 VALUES ('3-3333-3333',
-		'3-234-231233',
+		'1-222-333444',
 		'CR100002882910',
 		134000.5,
 		'Medio tiempo',
@@ -19,7 +21,5 @@ GO
 
 
 
-
 SELECT * FROM Persona;
 SELECT * FROM Beneficio;
-
