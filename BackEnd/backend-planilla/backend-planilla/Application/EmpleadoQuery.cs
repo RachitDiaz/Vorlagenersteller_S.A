b023cfb209de763895387df7f1a607582bb5fea7
Aldo Cascante <cascante.aldo@gmail.com>
2025-06-04 15:06:46 -0600

    validaciones para editar empleado

--- BackEnd/backend-planilla/backend-planilla/Application/EmpleadoQuery.cs
+++ BackEnd/backend-planilla/backend-planilla/Application/EmpleadoQuery.cs
@@ -17,6 +17,21 @@ namespace backend_planilla.Application
         }
         public bool EditarInfoEmpleado(InfoEmpleadoModel datosNuevos, string cedulaEmpleado)
         {
+            if (datosNuevos == null) return false;
+            InfoEmpleadoModel original;
+
+            try
+            {
+                original = ObtenerInfoEmpleado(cedulaEmpleado);
+                if (original == null) return false;
+            }
+            catch (Exception error)
+            {
+                throw;
+            }
+
+            if (!PermiteEdicion(original, datosNuevos)) return false;
+
             return _empleadoRepository.EditarInfoEmpleado(datosNuevos, cedulaEmpleado);
         }
         public InfoEmpleadoModel? ObtenerInfoEmpleado(string cedulaEmpleado)
@@ -26,6 +41,21 @@ namespace backend_planilla.Application
         public List<EmpleadoModel> ObtenerEmpleados(string correo)
         {
             return _empleadoRepository.ObtenerEmpleados(correo);
+        }
+
+        private static bool PermiteEdicion(InfoEmpleadoModel original, InfoEmpleadoModel nuevo)
+        {
+            if (original == null) return false;
+            if (nuevo.CedulaEditable != original.CedulaEditable) return false;
+            if (original.CedulaEditable) return true;
+
+            bool valido = true;
+            if (!original.Empleado.Nombre.Equals(nuevo.Empleado.Nombre)) valido = false;
+            if (!original.Empleado.Apellido1.Equals(nuevo.Empleado.Apellido1)) valido = false;
+            if (!original.Empleado.Apellido2.Equals(nuevo.Empleado.Apellido2)) valido = false;
+            if (!original.Empleado.CedulaEmpleado.Equals(nuevo.Empleado.CedulaEmpleado)) valido = false;
+
+            return valido;
         }
     }
 }
