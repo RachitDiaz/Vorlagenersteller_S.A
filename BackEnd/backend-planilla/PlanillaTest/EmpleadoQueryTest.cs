d5981c10337dbe462ac787f357c940e81035e379
Aldo Cascante <cascante.aldo@gmail.com>
2025-06-04 20:46:03 -0600

    unit testing

--- BackEnd/backend-planilla/PlanillaTest/EmpleadoQueryTest.cs
+++ BackEnd/backend-planilla/PlanillaTest/EmpleadoQueryTest.cs
@@ -22,27 +22,243 @@ namespace PlanillaTest
         public void EdicionValida()
         {
             string cedulaEmpleado = "1-2222-3333";
-            InfoEmpleadoModel entrada = new InfoEmpleadoModel();
+            InfoEmpleadoModel entrada = new InfoEmpleadoModel()
+            {
+                Empleado = new EmpleadoModel
+                {
+                    CedulaEmpleado = "1-2353-1241",
+                    CedulaEmpresa = "1-318-013913",
+                    Nombre = "Gerardo",
+                    Apellido1 = "Morales",
+                    Apellido2 = "Torrez",
+                    Banco = "BN1294019",
+                    SalarioBruto = 300000,
+                    TipoContrato = "Tiempo Completo"
+                },
+                Genero = "Masculino",
+                Correo = "alberto400@gmail.com",
+                CedulaEditable = true
+            };
             InfoEmpleadoModel original = new InfoEmpleadoModel()
             {
                 Empleado = new EmpleadoModel
                 {
-                    CedulaEmpleado = "1-2314-1241",
-                    CedulaEmpresa = "",
-                    Nombre = "", 
-                    Apellido1 = "",
-                    Apellido2 = "",
-                    Banco = "",
-                    SalarioBruto = 1,
-                    TipoContrato = ""
+                    CedulaEmpleado = "1-2222-3333",
+                    CedulaEmpresa = "1-318-013913",
+                    Nombre = "Alberto", 
+                    Apellido1 = "Morales",
+                    Apellido2 = "Torrez",
+                    Banco = "BN1294019",
+                    SalarioBruto = 10000,
+                    TipoContrato = "Tiempo Completo"
                 },
-                Genero = "",
-                Correo = "",
+                Genero = "Masculino",
+                Correo = "alberto313@gmail.com",
                 CedulaEditable = true
             };
 
             bool respuesta = true;
             bool resultadoEsperado = true;
+
+            _mockRepo.Setup(repo => repo.ObtenerInfoEmpleado(cedulaEmpleado)).Returns(original);
+            _mockRepo.Setup(repo => repo.EditarInfoEmpleado(entrada, cedulaEmpleado)).Returns(respuesta);
+
+            var resultado = _empleadoQuery.EditarInfoEmpleado(entrada, cedulaEmpleado);
+
+            Assert.That(resultadoEsperado, Is.EqualTo(resultado));
+        }
+
+        [Test]
+        public void NoEditable()
+        {
+            string cedulaEmpleado = "1-2222-3333";
+            InfoEmpleadoModel entrada = new InfoEmpleadoModel()
+            {
+                Empleado = new EmpleadoModel
+                {
+                    CedulaEmpleado = "1-2353-1241",
+                    CedulaEmpresa = "1-318-013913",
+                    Nombre = "Gerardo",
+                    Apellido1 = "Morales",
+                    Apellido2 = "Torrez",
+                    Banco = "BN1294019",
+                    SalarioBruto = 300000,
+                    TipoContrato = "Tiempo Completo"
+                },
+                Genero = "Masculino",
+                Correo = "alberto400@gmail.com",
+                CedulaEditable = false
+            };
+            InfoEmpleadoModel original = new InfoEmpleadoModel()
+            {
+                Empleado = new EmpleadoModel
+                {
+                    CedulaEmpleado = "1-2222-3333",
+                    CedulaEmpresa = "1-318-013913",
+                    Nombre = "Alberto",
+                    Apellido1 = "Morales",
+                    Apellido2 = "Torrez",
+                    Banco = "BN1294019",
+                    SalarioBruto = 10000,
+                    TipoContrato = "Tiempo Completo"
+                },
+                Genero = "Masculino",
+                Correo = "alberto313@gmail.com",
+                CedulaEditable = false
+            };
+
+            bool respuesta = true;
+            bool resultadoEsperado = false;
+
+            _mockRepo.Setup(repo => repo.ObtenerInfoEmpleado(cedulaEmpleado)).Returns(original);
+            _mockRepo.Setup(repo => repo.EditarInfoEmpleado(entrada, cedulaEmpleado)).Returns(respuesta);
+
+            var resultado = _empleadoQuery.EditarInfoEmpleado(entrada, cedulaEmpleado);
+
+            Assert.That(resultadoEsperado, Is.EqualTo(resultado));
+        }
+
+        [Test]
+        public void NoEditableValido()
+        {
+            string cedulaEmpleado = "1-2222-3333";
+            InfoEmpleadoModel entrada = new InfoEmpleadoModel()
+            {
+                Empleado = new EmpleadoModel
+                {
+                    CedulaEmpleado = "1-2222-3333",
+                    CedulaEmpresa = "1-318-013913",
+                    Nombre = "Alberto",
+                    Apellido1 = "Morales",
+                    Apellido2 = "Torrez",
+                    Banco = "BN1294019",
+                    SalarioBruto = 300000,
+                    TipoContrato = "Tiempo Completo"
+                },
+                Genero = "Masculino",
+                Correo = "alberto400@gmail.com",
+                CedulaEditable = false
+            };
+            InfoEmpleadoModel original = new InfoEmpleadoModel()
+            {
+                Empleado = new EmpleadoModel
+                {
+                    CedulaEmpleado = "1-2222-3333",
+                    CedulaEmpresa = "1-318-013913",
+                    Nombre = "Alberto",
+                    Apellido1 = "Morales",
+                    Apellido2 = "Torrez",
+                    Banco = "BN1294019",
+                    SalarioBruto = 10000,
+                    TipoContrato = "Tiempo Completo"
+                },
+                Genero = "Masculino",
+                Correo = "alberto313@gmail.com",
+                CedulaEditable = false
+            };
+
+            bool respuesta = true;
+            bool resultadoEsperado = true;
+
+            _mockRepo.Setup(repo => repo.ObtenerInfoEmpleado(cedulaEmpleado)).Returns(original);
+            _mockRepo.Setup(repo => repo.EditarInfoEmpleado(entrada, cedulaEmpleado)).Returns(respuesta);
+
+            var resultado = _empleadoQuery.EditarInfoEmpleado(entrada, cedulaEmpleado);
+
+            Assert.That(resultadoEsperado, Is.EqualTo(resultado));
+        }
+
+        [Test]
+        public void NoEditableInvalido()
+        {
+            string cedulaEmpleado = "1-2222-3333";
+            InfoEmpleadoModel entrada = new InfoEmpleadoModel()
+            {
+                Empleado = new EmpleadoModel
+                {
+                    CedulaEmpleado = "1-2122-3773",
+                    CedulaEmpresa = "1-318-013913",
+                    Nombre = "Fernando",
+                    Apellido1 = "Morales",
+                    Apellido2 = "Torrez",
+                    Banco = "BN1294019",
+                    SalarioBruto = 300000,
+                    TipoContrato = "Tiempo Completo"
+                },
+                Genero = "Masculino",
+                Correo = "alberto400@gmail.com",
+                CedulaEditable = false
+            };
+            InfoEmpleadoModel original = new InfoEmpleadoModel()
+            {
+                Empleado = new EmpleadoModel
+                {
+                    CedulaEmpleado = "1-2222-3333",
+                    CedulaEmpresa = "1-318-013913",
+                    Nombre = "Alberto",
+                    Apellido1 = "Morales",
+                    Apellido2 = "Torrez",
+                    Banco = "BN1294019",
+                    SalarioBruto = 10000,
+                    TipoContrato = "Tiempo Completo"
+                },
+                Genero = "Masculino",
+                Correo = "alberto313@gmail.com",
+                CedulaEditable = false
+            };
+
+            bool respuesta = true;
+            bool resultadoEsperado = false;
+
+            _mockRepo.Setup(repo => repo.ObtenerInfoEmpleado(cedulaEmpleado)).Returns(original);
+            _mockRepo.Setup(repo => repo.EditarInfoEmpleado(entrada, cedulaEmpleado)).Returns(respuesta);
+
+            var resultado = _empleadoQuery.EditarInfoEmpleado(entrada, cedulaEmpleado);
+
+            Assert.That(resultadoEsperado, Is.EqualTo(resultado));
+        }
+
+        [Test]
+        public void EditableDiscrepante()
+        {
+            string cedulaEmpleado = "1-2222-3333";
+            InfoEmpleadoModel entrada = new InfoEmpleadoModel()
+            {
+                Empleado = new EmpleadoModel
+                {
+                    CedulaEmpleado = "1-2222-3333",
+                    CedulaEmpresa = "1-318-013913",
+                    Nombre = "Gerardo",
+                    Apellido1 = "Morales",
+                    Apellido2 = "Torrez",
+                    Banco = "BN1294019",
+                    SalarioBruto = 300000,
+                    TipoContrato = "Tiempo Completo"
+                },
+                Genero = "Masculino",
+                Correo = "alberto400@gmail.com",
+                CedulaEditable = true
+            };
+            InfoEmpleadoModel original = new InfoEmpleadoModel()
+            {
+                Empleado = new EmpleadoModel
+                {
+                    CedulaEmpleado = "1-2222-3333",
+                    CedulaEmpresa = "1-318-013913",
+                    Nombre = "Alberto",
+                    Apellido1 = "Morales",
+                    Apellido2 = "Torrez",
+                    Banco = "BN1294019",
+                    SalarioBruto = 10000,
+                    TipoContrato = "Tiempo Completo"
+                },
+                Genero = "Masculino",
+                Correo = "alberto313@gmail.com",
+                CedulaEditable = false
+            };
+
+            bool respuesta = true;
+            bool resultadoEsperado = false;
 
             _mockRepo.Setup(repo => repo.ObtenerInfoEmpleado(cedulaEmpleado)).Returns(original);
             _mockRepo.Setup(repo => repo.EditarInfoEmpleado(entrada, cedulaEmpleado)).Returns(respuesta);
