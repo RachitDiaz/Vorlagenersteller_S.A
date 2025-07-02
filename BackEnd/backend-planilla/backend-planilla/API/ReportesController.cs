13c599e244a7aeaf41608d66a1535058f0b308bb
Aldo Cascante <cascante.aldo@gmail.com>
2025-07-02 08:21:35 -0600

    correcciones para correcto funcionamiento

--- BackEnd/backend-planilla/backend-planilla/API/ReportesController.cs
+++ BackEnd/backend-planilla/backend-planilla/API/ReportesController.cs
@@ -94,7 +94,7 @@ namespace backend_planilla.Controllers
                 if (string.IsNullOrWhiteSpace(_correoUsuario))
                     return BadRequest();
 
-                _correoUsuario = "cascante.aldo@gmail.com";
+                
                 _ReportesQuery.enviarEmailReporte(documentoPDF, _correoUsuario);
                 
             }
