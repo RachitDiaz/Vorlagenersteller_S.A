e59d5c29c1500ff94cd9e54cd6c57c26a4888c4e
Jesus <jesus.menaamador@ucr.ac.cr>
2025-07-03 01:32:15 -0600

    Se agregan variables aparte para el correo y contraseña de prueba

--- BackEnd/backend-planilla/PlanillaTest/UITests/LoginTestUI.cs
+++ BackEnd/backend-planilla/PlanillaTest/UITests/LoginTestUI.cs
@@ -19,8 +19,10 @@ namespace PlanillaTest.UITests
         }
 
         [Test]
-        public void Enter_To_List_Of_Countries_Test()
+        public void LoginUITest()
         {
+            string correoPrueba = "shihtangdaniel@gmail.com";
+            string contrasenaPrueba = "ContraTemporal";
             var url = "http://localhost:8080/";
             _driver.Manage().Window.Maximize();
             _driver.Navigate().GoToUrl(url);
@@ -30,17 +32,15 @@ namespace PlanillaTest.UITests
             _wait.Until(d => d.FindElement(By.CssSelector(".tab:nth-child(2)"))).Click();
 
             _wait.Until(d => d.FindElement(By.CssSelector(".form-group:nth-child(1) > input"))).Click();
-            _driver.FindElement(By.CssSelector(".form-group:nth-child(1) > input")).SendKeys("shihtangdaniel@gmail.com");
+            _driver.FindElement(By.CssSelector(".form-group:nth-child(1) > input")).SendKeys(correoPrueba);
 
             _driver.FindElement(By.CssSelector(".form-group:nth-child(2) > input")).Click();
-            _driver.FindElement(By.CssSelector(".form-group:nth-child(2) > input")).SendKeys("ContraTemporal");
+            _driver.FindElement(By.CssSelector(".form-group:nth-child(2) > input")).SendKeys(contrasenaPrueba);
 
             _driver.FindElement(By.CssSelector(".login-button")).Click();
 
             var h2 = _wait.Until(d => d.FindElement(By.CssSelector("h2")));
             Assert.That(h2.Text, Is.EqualTo("Gestión de Empleados"));
-
-           // _driver.FindElement(By.CssSelector(".auth-button")).Click();
         }
 
         [TearDown]
